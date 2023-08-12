1eb775763d977e3a5533cd3f56dbf605fde0030c
null <9310d27e@gmail.com>
2023-08-12 12:46:24 +0400

    1

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'API',
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  base_url: process.env.BASE_URL || 'http://backend:3000',
+  base_url: process.env.BASE_URL || 'http://dnode_backend:3000',
   db: {
-    uri: process.env.REDIS_URI || 'redis://redis:6379',
+    uri: process.env.REDIS_URI || 'redis://dnode_redis:6379',
   },
 };
